a9856777898cafea425360621ccb3538f6328aad
Thomas Kessler <kesslerthomas@kloee.com>
2016-12-13 10:50:19 -0500

    webpack test

---
+++ README.md
@@ -0,0 +1,25 @@
+# Angular 2 + Express + Stormpath Example
+
+<div>
+  <a href="http://angular.io">
+    <img src="https://github.com/stormpath/angular2-express-stormpath-example/raw/master/static/angular.png">
+  </a>
+  <a href="http://expressjs.com/">
+      <img src="https://github.com/stormpath/angular2-express-stormpath-example/raw/master/static/express.png">
+  </a>
+  <a href="https://www.stormpath.com/">
+    <img src="https://github.com/stormpath/angular2-express-stormpath-example/raw/master/static/stormpath.png">
+  </a>
+</div>
+
+This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.19-3. It's an example application that shows
+a typical [Angular 2](http://angular.io/) webapp that authenticates with [Stormpath](https://stormpath.com) using an [Express](http://expressjs.com/) backend.
+
+You can read about how this application was created on [the Stormpath blog](https://stormpath.com/blog/angular-2-user-authentication). Feel free to copy any code 
+in this project for your own use in accordance with the [Apache 2.0 license](LICENSE).
+
+**Prerequisites**: Node.js, a [Stormpath Account](https://api.stormpath.com/register), and an `apiKey.properties` file in `~/stormpath/`.
+
+To run the app, execute `npm install && npm start`. 
+
+To learn more, see Stormpath's [Angular 2 SDK](https://github.com/stormpath/stormpath-sdk-angular) and [Express Stormpath](https://github.com/stormpath/express-stormpath).
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    "app": "./src/main"
+  },
+  output: {
+    path: "./src",
+    filename: "./app.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+  devServer: {
+    port: 4200,
+    proxy: {
+      '/forgot': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+      '/login': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+      '/logout': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+      '/me': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+      '/register': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+      "/token/**": {
+        "target": "https://localhost:3000",
+        "secure": false,
+        "changeOrigin": true
+      }
+    }
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.ts/,
+        loaders: ['ts-loader'],
+        exclude: /node_modules/
+      }
+      // {
+      //   test: /\.ts$/,
+      //   loaders: ['awesome-typescript-loader', 'angular2-template-loader?keepUrl=true'],
+      //   exclude: [/\.(spec|e2e)\.ts$/, /node_modules/]
+      // },
+      // /* Embed files. */
+      // {
+      //   test: /\.(html|css)$/,
+      //   loader: 'raw-loader',
+      //   exclude: /\.async\.(html|css)$/
+      // },
+      // /* Async loading. */
+      // {
+      //   test: /\.async\.(html|css)$/,
+      //   loaders: ['file?name=[name].[hash].[ext]', 'extract']
+      // }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require('./package.json').version)
+    })
+  ]
+}
